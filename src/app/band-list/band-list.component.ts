cf79f7877532221b700b04fab50b586c034fe26e
yazidT <t.yazid@live.fr>
2021-12-21 19:54:00 +0100

    fist lesson operators

--- src/app/band-list/band-list.component.ts
+++ src/app/band-list/band-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { BandDataService } from '../band-data.service';
 import { Band } from '../model';
 
@@ -11,6 +13,15 @@ export class BandListComponent implements OnInit {
   bandList: Band[] | undefined;
 
   constructor(private bandDataService: BandDataService) {
+    const o = interval(1000).pipe(
+      map(v => v * v),
+      filter(v => v %2 == 0)
+    )
+    
+
+    o.subscribe(
+      (value)=>{console.log(value)}
+    );
     this.bandDataService.getBands().subscribe(bands => {
       this.bandList = bands;
     });
