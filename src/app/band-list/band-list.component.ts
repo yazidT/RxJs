4deb8325eebaba45cbf0ef69dd3a1ade93e1a069
yazidT <t.yazid@live.fr>
2021-12-22 21:10:25 +0100

    subjects and merge map

--- src/app/band-list/band-list.component.ts
+++ src/app/band-list/band-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { interval, Observable, Subject } from 'rxjs';
+import { filter, map, mergeMap, startWith } from 'rxjs/operators';
 import { BandDataService } from '../band-data.service';
 import { Band } from '../model';
 
@@ -10,14 +10,25 @@ import { Band } from '../model';
   styleUrls: ['./band-list.component.css']
 })
 export class BandListComponent implements OnInit {
+
+  refreshDataClickSubject = new Subject();  // subject is an observable that u can inject methods on it
+
   bandList$: Observable<Band[]> | undefined;
 
   constructor(private bandDataService: BandDataService) {
+    const refreshDataClick$ = this.refreshDataClickSubject.asObservable();
 
-    this.bandList$= this.bandDataService.getBands();
+    
+    this.bandList$= refreshDataClick$.pipe(
+      startWith({}),                                       //methode 1 its the equivalent of the fist click to load the datas
+      mergeMap(()=> this.bandDataService.getBands()         // Merge Map is equivalent to merge tow oservables because map returns an $ 
+      )
+    );
   }
 
   ngOnInit(): void {
+    //this.bandList$ = this.bandDataService.getBands();   //methode 2 Initialize the data with the ngOnInit
+    
   }
 
 
